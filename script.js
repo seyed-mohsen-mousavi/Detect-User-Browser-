587e46153a3ce089e55cf4a029fc5bafb2202685
mr.root <1mr.rooter3@gmail.com>
2024-03-01 01:03:32 +0330

    up to date

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ window.onload = () => {
 function getbatteryInfo(i) {
   console.log(i);
   isChar();
-  i.onchargingchange = isChar();
+  i.addEventListener("chargingchange", () => {
+    isChar();
+  });
   let power = i.level * 100;
   let statusColor;
   if (power > 80 && power) {
@@ -28,20 +30,21 @@ function getbatteryInfo(i) {
   showPower.style.color = statusColor;
   showPower.innerHTML = power + "%";
   liquidBattery.style.height = power + "%";
+  liquidBattery.style.backgroundColor = statusColor;
   battery.style.display = "inline-block";
   function isChar() {
     if (i.charging) {
       chargingElm.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20px">
         <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
       </svg>
-      The device is charging `;
+      دستگاه در حال شارژ است `;
       chargingElm.style.color = "#71fb85";
     } else {
       chargingElm.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="20px">
         <path stroke-linecap="round" stroke-linejoin="round" d="M11.412 15.655 9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457 3 3m5.457 5.457 7.086 7.086m0 0L21 21" />
       </svg>
       
-      The device is not charging `;
+      دستگاه در حال شارژ نیست `;
       chargingElm.style.color = "#fb7171";
     }
   }
